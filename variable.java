50fb4872c76ae0b46f283bb789e4911156af2142
unknown <allen0pig@gmail.com>
2021-11-16 13:46:18 +0800

    second week

---
+++ variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    public static void main(String[] args) {
+        float hight = 1.92f ;
+        int weight = 110 ;
+        System.out.println(weight/(hight*hight));
+    }
+
+}

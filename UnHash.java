b3e738ecbfae2cf73bef57a23a38c6467a366760
allen00pig <allen0pig@gmail.com>
2021-11-23 15:57:01 +0800

    2021/11/23

--- UnHash.java
+++ UnHash.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class UnHash {
+class UnHash{
+
+    public static void main (String[] args) {
+        System.out.println(revHash(6933552791181934L));
+        System.out.println(hash("justdoit")); //574318821802
+    }
+
+    public static String letters = "cdefghijlmnoqstuvxz";
+
+    public static long revHash(long hash) {
+        long  s = hash ;
+        long h = 0L;
+        for (int i = 0; i < 20 ; i++){
+            h = s % 23 ;
+            s = (s - h) / 23;
+            char ans = letters.charAt((int) h);
+            System.out.println(ans);
+            if(s == 7){
+                break;
+            }
+        }
+        return s;
+
+
+    }
+
+    public static long hash(String s){
+        long h = 7;
+        for (int i = 0; i < s.length(); i++){
+            h = h * 23 + letters.indexOf(s.charAt(i));
+        }
+
+        return h;
+    }
 }

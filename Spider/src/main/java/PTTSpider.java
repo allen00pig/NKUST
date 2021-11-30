3e58f55f67e84f89e8ffc8c55a52140a750cbeee
allen00pig <allen0pig@gamil.com>
2021-11-30 16:25:16 +0800

    11/30 updata

--- Spider/src/main/java/PTTSpider.java
+++ Spider/src/main/java/PTTSpider.java
@@ -1,5 +1,21 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class PTTSpider {
     public static void main(String[] args) {
-
+        try {
+            Document doc = Jsoup.connect("https://www.ptt.cc/bbs/Gossiping/M.1638258698.A.66E.html").get();
+            //System.out.println(doc.title());
+            Elements newsHeadlines = doc.select("#main-content > div:nth-child(7)");
+            for (Element headline : newsHeadlines) {
+                System.out.println(headline.attr("title"));
+                System.out.println(headline.absUrl("href"));
+            }
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
     }
 }
